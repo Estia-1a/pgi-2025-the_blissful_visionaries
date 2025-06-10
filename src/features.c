ebcd4c222cb9cd4551eb648eba4f2c97c0e37730
Noé <noe.mabillot@etu.estia.fr>
2025-06-10 09:20:06 +0200

    First pixel

--- src/features.c
+++ src/features.c
@@ -5,26 +5,33 @@
 #include "features.h"
 #include "utils.h"
 
-void helloWorld() {
+void helloWorld()
+{
     printf("Hello World !");
 }
-void dimension(char* filename) {
-    unsigned char* data;
+void dimension(char *filename)
+{
+    unsigned char *data;
     int width, height, channel_count;
-    if (read_image_data(filename, &data, &width, &height, &channel_count) == 0) {
+    if (read_image_data(filename, &data, &width, &height, &channel_count) == 0)
+    {
         printf("Erreur avec le fichier : %s\n", filename);
-    } else {
+    }
+    else
+    {
         printf("dimension: %d, %d\n", width, height);
         free_image_data(data);
     }
 }
-void tenth_pixel(char *source_path) {
+void tenth_pixel(char *source_path)
+{
     unsigned char *data;
     int width, height, channel_count;
-    if (read_image_data(source_path, &data, &width, &height, &channel_count) == 0) {
+    if (read_image_data(source_path, &data, &width, &height, &channel_count) == 0)
+    {
         printf("Erreur avec le fichier : %s\n", source_path);
     }
-    
+
     int x = 9;
     int y = 0;
     int pixel_index = (y * width + x) * channel_count;
@@ -34,5 +41,26 @@ void tenth_pixel(char *source_path) {
     int B = data[pixel_index + 2];
 
     printf("tenth_pixel: %d, %d, %d\n", R, G, B);
+    free_image_data(data);
+}
+
+void first_pixel(char *source_path)
+{
+    unsigned char *data;
+    int width, height, channel_count;
+    if (read_image_data(source_path, &data, &width, &height, &channel_count) == 0)
+    {
+        printf("Erreur avec le fichier : %s\n", source_path);
+    }
+
+    int x = 0;
+    int y = 0;
+    int pixel_index = (y * width + x) * channel_count;
+
+    int R = data[pixel_index];
+    int G = data[pixel_index + 1];
+    int B = data[pixel_index + 2];
+
+    printf("first_pixel: %d, %d, %d\n", R, G, B);
     free_image_data(data);
 }
